04342da904ec38b8962cc804e6d2e9074cfbd066
kbasa <kbasa@DESKTOP-JPD3MIN>
2017-01-24 02:13:08 +0200

    Commit3

--- HQOrderEntryImplementation/HQOrderEntryService.cs
+++ HQOrderEntryImplementation/HQOrderEntryService.cs
@@ -1,18 +1,120 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel.MsmqIntegration;
 using System.Text;
 using System.Threading.Tasks;
+using HQOrderEntryServiceInterface;
+using LocalOrderEntryInterface;
+using HelperLib;
 
 namespace HQOrderEntryImplementation
 {
-    public class HQOrderEntryService //: HQOrderEntryServiceInterface.IOrderEntryService
+    public class HQOrderEntryService : IOrderEntryService
     {
+        public void SendOrderEntry(MsmqMessage<HQOrderEntry> orderEntryMsg)
+        {
+            try
+            {
+                if (CheckIfOrderIsValid(orderEntryMsg.Body))
+                {
 
-        //public void SendOrderEntry
-        //(MsmqMessage<HQOrderEntryServiceInterface.HQOrderEntry> orderEntryMsg)
-        //{
-            
-        //}
+                }
+                else
+                {
+                    SendToInvalidOrderQueue(orderEntryMsg);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private bool CheckIfOrderIsValid(HQOrderEntry orderEntry)
+        {
+            HQProductServiceASMXClient.HQProductServiceASMXSoapClient client;
+            client = new HQProductServiceASMXClient.HQProductServiceASMXSoapClient();
+
+            bool orderIsValid = true;
+
+            foreach (var item in orderEntry.OrderOrderedProducts)
+            {
+                orderIsValid = client.IsProductAvailableForCountry(item.ProductID, orderEntry.OrderCustomer.CustomerCountryCode);
+            }
+            return orderIsValid;
+        }
+
+        private LocalOrderEntry ConvertOrderEntrySchema(HQOrderEntry orderEntry)
+        {
+            LocalOrderEntry localOrderEntry;
+            localOrderEntry = new LocalOrderEntry();
+
+            localOrderEntry.CustomerName =
+            orderEntry.OrderCustomer.CustomerName;
+            localOrderEntry.CustomerAddressLine1 =
+            orderEntry.OrderCustomer.CustomerAddressLine1;
+            localOrderEntry.CustomerAddressLine2 =
+            orderEntry.OrderCustomer.CustomerAddressLine2;
+            localOrderEntry.CustomerCountryCode =
+            orderEntry.OrderCustomer.CustomerCountryCode;
+
+            localOrderEntry.OrderOrderedProducts =
+            new List<LocalOrderEntryInterface.OrderedProducts>();
+
+            foreach (var item in orderEntry.OrderOrderedProducts)
+            {
+                string translation;
+                translation = TranslateProductDescription(item.ProductID,
+                orderEntry.OrderCustomer.CustomerCountryCode);
+
+                localOrderEntry.OrderOrderedProducts.Add(
+                new LocalOrderEntryInterface.OrderedProducts
+                {
+                    ProductID = orderEntry.OrderCustomer.CustomerCountryCode
+                + "/"
+                + item.ProductID,
+                    Quantity = item.Quantity,
+                    LocalizedDescription = translation
+                });
+            }
+            return localOrderEntry;
+        }
+
+        private void RouteOrderEntry(LocalOrderEntry localOrderEntry)
+        {
+            //Add code later
+        }
+
+        private void SendToInvalidOrderQueue(MsmqMessage<HQOrderEntry> orderEntryMsg)
+        {
+            //Add code later
+        }
+
+        private string TranslateProductDescription(string productID, string languageCode)
+        {
+            System.Net.HttpWebRequest webrequest;
+            webrequest = (System.Net.HttpWebRequest)System.Net.HttpWebRequest.Create(
+            string.Format
+            (@"http://localhost:8081/HQLocalizationService/TranslateProductDescriptions/{0}/{1}",
+            languageCode,
+            productID));
+            webrequest.ContentLength = 0;
+
+            System.Net.HttpWebResponse webresponse;
+
+            webresponse = (System.Net.HttpWebResponse)webrequest.GetResponse();
+
+            Encoding enc = System.Text.Encoding.GetEncoding(1252);
+            System.IO.StreamReader loResponseStream = new
+            System.IO.StreamReader(webresponse.GetResponseStream(), enc);
+
+            string response = loResponseStream.ReadToEnd();
+
+            string answer;
+            answer = GenericSerializer<string>.DeserializeDC(response);
+
+            return answer;
+        }
     }
 }
---
+++ LocalOrderEntryInterface/IReceiveOrderEntryLocalBranch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalOrderEntryInterface
+{
+    [ServiceContract]
+    public interface IReceiveOrderEntryLocalBranch
+    {
+        [OperationContract]
+        int SendLocalOrderEntry(LocalOrderEntry localOrderEntry);
+    }
+
+    [DataContract]
+    public class LocalOrderEntry
+    {
+        [DataMember]
+        public String OrderEntryID { get; set; }
+        [DataMember]
+        public DateTime OrderEntryDate { get; set; }
+        [DataMember]
+        public string CustomerName { get; set; }
+        [DataMember]
+        public string CustomerAddressLine1 { get; set; }
+        [DataMember]
+        public string CustomerAddressLine2 { get; set; }
+        [DataMember]
+        public string CustomerCountryCode { get; set; }
+        [DataMember]
+        public List<OrderedProducts> OrderOrderedProducts { get; set; }
+    }
+
+    [DataContract]
+    public class OrderedProducts
+    {
+        [DataMember]
+        public string ProductID { get; set; }
+        [DataMember]
+        public int Quantity { get; set; }
+        [DataMember]
+        public string LocalizedDescription { get; set; }
+    }
+}
